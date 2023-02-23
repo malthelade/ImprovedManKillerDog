04ad5bf4c9a589f665cf454407d2558726398d64
malthelade <ladefogedmalthe@gmail.com>
2023-02-23 10:40:59 +0100

    Ep 19

--- src/SoftCollision.gd
+++ src/SoftCollision.gd
@@ -9,4 +9,6 @@ func get_push_vector():
 	var push_vector = Vector2.ZERO
 	if is_colliding():
 		var area = areas[0]
-		push_vector = global_position.direction_to(global_position)
+		push_vector = area.global_position.direction_to(global_position)
+		push_vector = push_vector.normalized()
+	return push_vector
