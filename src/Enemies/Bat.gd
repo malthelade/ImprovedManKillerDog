04a8144e733ef17f817afb8f61d72b4d1043a174
Kristian <krisgamer@live.com>
2023-03-16 08:48:58 +0100

    Spawn chance of potions

--- src/Enemies/Bat.gd
+++ src/Enemies/Bat.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
+const PotionSpawn = preload("res://Items/Potion.tscn")
 
 export var ACCELERATION = 300
 export var MAX_SPEED = 50
@@ -88,7 +89,20 @@ func _on_Hurtbox_area_entered(area):
 	hurtbox.create_hit_effect()
 
 func _on_Stats_no_health():
+	
 	queue_free()
 	var enemyDeathEffect = EnemyDeathEffect.instance()
 	get_parent().add_child(enemyDeathEffect)
 	enemyDeathEffect.global_position = global_position
+	
+	spawn_potion(0.15)
+	
+
+
+
+func spawn_potion(threshold):
+	
+	if randf()>threshold:
+		var potionSpawn = PotionSpawn.instance()
+		get_parent().add_child(potionSpawn)
+		potionSpawn.global_position = global_position
